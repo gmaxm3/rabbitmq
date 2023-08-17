955d4c6ee890e0631fe9965832a031de4db0ef88
Gorinov Maxim <39081283+gmaxm3@users.noreply.github.com>
2023-08-17 17:24:42 +0300

    Update send.py

--- send.py
+++ send.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters( 'localhost' ))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
+channel.queue_declare(queue='Hello, friend. How are you?')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
